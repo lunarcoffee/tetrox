1b58c6b0618965cc18065baa88d50b1e0a099224
LunarCoffee <lunarcoffee.pjc@gmail.com>
2022-03-31 19:28:27 -0400

    clean up config a bit

--- client/src/config.rs
+++ client/src/config.rs
@@ -56,6 +56,7 @@ impl Deref for ReadOnlyConfig {
 
 pub enum ConfigMessage {
     SkinName(String),
+    FieldZoom(f64),
 
     FieldWidth(usize),
     FieldHeight(usize),
@@ -71,6 +72,43 @@ pub struct ConfigPanelWrapper {
     config: Config,
 }
 
+impl ConfigPanelWrapper {
+    fn section_heading(name: &str) -> Html {
+        html! { <p class="config-heading">{ name }</p> }
+    }
+
+    fn range_input<T>(label: &str, min: T, max: T, step: T, value: T, callback: Callback<InputEvent>) -> Html
+    where
+        T: fmt::Display,
+    {
+        html! {
+            <div class="config-option">
+                { Self::input_label(label, &value) }
+                <input type="range" min={ min.to_string() } max={ max.to_string() } step={ step.to_string() }
+                       value={ value.to_string() } 
+                       oninput={ callback }/>
+            </div>
+        }
+    }
+
+    fn select_input(label: &str, items: &[&'static str], selected: &str, callback: Callback<InputEvent>) -> Html {
+        html! {
+            <div class="config-option">
+                { Self::input_label(label, &selected) }
+                <select oninput={ callback }>{
+                    for items.iter().map(|i| {
+                        html! { <option value={ *i } selected={ selected == *i }>{ i }</option> }
+                    })
+                }</select>
+            </div>
+        }
+    }
+
+    fn input_label(label: &str, value: &impl fmt::Display) -> Html {
+        html! { <p class="config-option-label">{ format!("{} ({}):", label, value) }</p> }
+    }
+}
+
 impl Component for ConfigPanelWrapper {
     type Message = ConfigMessage;
     type Properties = ();
@@ -84,12 +122,15 @@ impl Component for ConfigPanelWrapper {
     fn update(&mut self, _ctx: &Context<Self>, msg: Self::Message) -> bool {
         match msg {
             ConfigMessage::SkinName(skin_name) => self.config.skin_name = skin_name,
+            ConfigMessage::FieldZoom(zoom) => self.config.field_zoom = zoom,
+
             ConfigMessage::FieldWidth(width) => self.config.field_width = width,
             ConfigMessage::FieldHeight(height) => {
                 self.config.field_height = height;
                 self.config.field_hidden = height / 2;
             }
             ConfigMessage::QueueLen(queue_len) => self.config.queue_len = queue_len,
+
             ConfigMessage::DelayedAutoShift(das) => self.config.delayed_auto_shift = das,
             ConfigMessage::AutoRepeatRate(arr) => self.config.auto_repeat_rate = arr,
             ConfigMessage::SoftDropRate(sdr) => self.config.soft_drop_rate = sdr,
@@ -110,6 +151,7 @@ impl Component for ConfigPanelWrapper {
         }
 
         let skin_name_callback = make_update_callback!(HtmlSelectElement, ConfigMessage::SkinName);
+        let field_zoom_callback = make_update_callback!(HtmlInputElement, ConfigMessage::FieldZoom);
         let field_width_callback = make_update_callback!(HtmlInputElement, ConfigMessage::FieldWidth);
         let field_height_callback = make_update_callback!(HtmlInputElement, ConfigMessage::FieldHeight);
         let queue_len_callback = make_update_callback!(HtmlInputElement, ConfigMessage::QueueLen);
@@ -117,45 +159,25 @@ impl Component for ConfigPanelWrapper {
         let arr_callback = make_update_callback!(HtmlInputElement, ConfigMessage::AutoRepeatRate);
         let sdr_callback = make_update_callback!(HtmlInputElement, ConfigMessage::SoftDropRate);
 
-        let skin_name = &self.config.skin_name;
-        let field_width = self.config.field_width;
-        let field_height = self.config.field_height;
-        let queue_len = self.config.queue_len;
-        let das = self.config.delayed_auto_shift.to_string();
-        let arr = self.config.auto_repeat_rate.to_string();
-        let sdr = self.config.soft_drop_rate.to_string();
+        let config = &self.config;
 
         html! {
             <div class="content">
                 <Board config={ ReadOnlyConfig(self.config.clone()) }/>
                 <div class="config-panel">
-                    <p class="config-heading">{ "Options" }</p>
-                    <p class="config-option-label">{ format!("Block skin ({}):", skin_name) }</p>
-                    <select oninput={ skin_name_callback }>{
-                        for crate::SKIN_NAMES.iter().map(|skin| {
-                            html! { <option value={ *skin } selected={ skin_name == *skin }>{ skin }</option> }
-                        })
-                    }</select>
+                    { Self::section_heading("Visual") }
+                    { Self::select_input("Block skin", crate::SKIN_NAMES, &config.skin_name, skin_name_callback) }
+                    { Self::range_input("Field zoom", 0.25, 4.0, 0.01, config.field_zoom, field_zoom_callback) }
 
-                    <p class="config-option-label">{ format!("Field width ({}):", field_width) }</p>
-                    <input type="range" min="4" max="100" value={ field_width.to_string() }
-                           oninput={ field_width_callback }/>
+                    { Self::section_heading("Playfield") }
+                    { Self::range_input("Field width", 4, 100, 1, config.field_width, field_width_callback) }
+                    { Self::range_input("Field height", 6, 100, 1, config.field_height, field_height_callback) }
+                    { Self::range_input("Queue length", 0, 7, 1, config.queue_len, queue_len_callback) }
 
-                    <p class="config-option-label">{ format!("Field height ({}):", field_height) }</p>
-                    <input type="range" min="6" max="100" value={ field_height.to_string() }
-                           oninput={ field_height_callback }/>
-
-                    <p class="config-option-label">{ format!("Queue length ({}):", queue_len) }</p>
-                    <input type="range" min="0" max="7" value={ queue_len.to_string() } oninput={ queue_len_callback }/>
-
-                    <p class="config-option-label">{ format!("DAS ({} ms):", das) }</p>
-                    <input type="range" min="0" max="500" value={ das } oninput={ das_callback }/>
-
-                    <p class="config-option-label">{ format!("ARR ({} ms):", arr) }</p>
-                    <input type="range" min="0" max="500" value={ arr } oninput={ arr_callback }/>
-
-                    <p class="config-option-label">{ format!("SDR ({} ms):", sdr) }</p>
-                    <input type="range" min="0" max="500" value={ sdr } oninput={ sdr_callback }/>
+                    { Self::section_heading("Handling") }
+                    { Self::range_input("DAS", 0, 500, 1, config.delayed_auto_shift, das_callback) }
+                    { Self::range_input("ARR", 0, 500, 1, config.auto_repeat_rate, arr_callback) }
+                    { Self::range_input("SDR", 0, 500, 1, config.soft_drop_rate, sdr_callback) }
                 </div>
             </div>
         }
